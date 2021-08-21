75a6b88ccaedb6b0eca7ddd05a124d55f77b7e19
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-21 10:42:08 +0300

    added the players details by mosa and the team events by ashrf

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const getallLeaguesController=require('./Controllers/getallLeagues.controller')
-const getallTeamesController=require('./Controllers/getAllTeamsInLeague.controller')
+const getallTeamesController=require('./Controllers/getAllTeamsInLeague.controller');
+const getTeamEventsById=require('./Controllers/getEventsByTeamId.controller')
+const getPlayersController=require('./Controllers/getPlayersController.controller')
 const cors = require("cors");
 app.use(cors());
 
@@ -12,27 +14,9 @@ const PORT = process.env.PORT;
 
 
 app.get("/leagues/:countryName",getallLeaguesController);
-
+app.get("/player/:playerId",getPlayersController);
 app.get("/teams/:leagueId",getallTeamesController);
-
-// app.get("/home/team", (req, res) => {
-//   let name = req.query.name;
-//   axios
-//     .get(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${name}`)
-//     .then((resp) => {
-//       res.json(resp.data);
-//     });
-// });
-// app.get("/home/player", (req, res) => {
-//   let name = req.query.name;
-//   axios
-//     .get(
-//       `https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${name}`
-//     )
-//     .then((resp) => {
-//       res.json(resp.data);
-//     });
-// });
+app.get("/teamEvents/:teamId",getTeamEventsById);
 
 app.listen(PORT, () => {
   console.log("ALIVE");
