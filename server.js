2f3324562db30b659a7bdefcff96134381eb2bf7
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-22 17:45:00 +0300

    test

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const getallTeamesController = require('./Controllers/getAllTeamsInLeague.contro
 const getallTeamData = require ('./Controllers/getallTeamData')
 const getTeamEventsById=require('./Controllers/getEventsByTeamId.controller')
 const getPlayersController=require('./Controllers/getPlayersController.controller')
-
+const {createUser,updateUser,getUsers,removeUser,showUser}=require('./Controllers/dataBase')
 const cors = require("cors");
 app.use(cors());
-
+app.use(express.json())
 require("dotenv").config();
 const PORT = process.env.PORT;
 
@@ -24,6 +24,14 @@ app.get("/lookup/:idTeam", getallTeamData);
 app.get("/player/:playerId",getPlayersController);
 app.get("/teamEvents/:teamId",getTeamEventsById);
 
+
+
+/////////////////////////////////
+app.get('/all',getUsers)
+app.get('/user/:id',showUser)
+app.post('/Createuser',createUser)
+app.patch('/user/fav/:id',updateUser)
+app.delete('/removeUser/:id',removeUser)
 
 app.listen(PORT, () => {
   console.log("ALIVE");
